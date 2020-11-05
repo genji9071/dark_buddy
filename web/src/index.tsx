a63dbd398e574ff7cb2925925a7ff2dbb7505494
tianhaozhang <zhangtianhao@tezign.com>
2020-11-05 17:47:37 +0800

    开新坑，做页面

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import Root from './pages/Root';
 import VConsole from "vconsole";
+import ChatApp from './pages/Chat'
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+ReactDOM.render(<ChatApp />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
 
+
+
 if (process.env.REACT_APP_ENV !== "production") {
   new VConsole();
+  
 }
