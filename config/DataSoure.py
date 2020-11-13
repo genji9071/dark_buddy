7703aa729ab9e76cf71e90d7de70ec9a95877875
tianhaozhang <zhangtianhao@tezign.com>
2020-11-13 16:01:58 +0800

    gitlab-ci部署

--- config/DataSoure.py
+++ config/DataSoure.py
@@ -3,12 +3,14 @@
 # 数据库连接配置
 
 # mysql配置
-host = 'host'
-port_mysql = 3306
-user_mysql = 'user_mysql'
-password_mysql = 'password_mysql'
-db_mysql = 'db_mysql'
+import os
+
+host = os.environ.get("MYSQL_HOST")
+port_mysql = int(os.environ.get("MYSQL_PORT"))
+user_mysql = os.environ.get("MYSQL_USERNAME")
+password_mysql = os.environ.get("MYSQL_PASSWORD")
+db_mysql = os.environ.get("MYSQL_DBNAME")
 
 # redis配置
-port_redis = 6379
-password_redis = "password_redis"
+port_redis = int(os.environ.get("REDIS_PORT"))
+password_redis = os.environ.get("REDIS_PASSWORD")
