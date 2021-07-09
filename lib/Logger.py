482471b3815dc30f86ea0dd4ef649af5e605262a
tianhaozhang <zhangtianhao@tezign.com>
2021-07-09 14:21:44 +0800

    移除重复log的问题

--- lib/Logger.py
+++ lib/Logger.py
@@ -10,21 +10,20 @@ class Logger:
     def __init__(self):
         # create logger
         logger = logging.getLogger('console')
-        logger.setLevel(logging.DEBUG)
 
-        # create console handler and set level
-        handler = logging.StreamHandler()
-        handler.setLevel(logging.DEBUG)
-
-        # log format
-        _format = '%(asctime)s  %(levelname)s %(process)d --- [%(threadName)+15s] module.%(module)-20s : %(message)s'
-        formatter = logging.Formatter(_format, datefmt='%Y-%m-%d %H:%M:%S.000')
-
-        # set formatter
-        handler.setFormatter(formatter)
-
-        # set handler
-        logger.addHandler(handler)
+        # # create console handler and set level
+        # handler = logging.StreamHandler()
+        # handler.setLevel(logging.DEBUG)
+        #
+        # # log format
+        # _format = '%(asctime)s  %(levelname)s %(process)d --- [%(threadName)+15s] module.%(module)-20s : %(message)s'
+        # formatter = logging.Formatter(_format, datefmt='%Y-%m-%d %H:%M:%S.000')
+        #
+        # # set formatter
+        # handler.setFormatter(formatter)
+        #
+        # # set handler
+        # logger.addHandler(handler)
         self.logger = logger
 
 
